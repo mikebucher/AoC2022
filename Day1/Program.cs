e4dd3c65b2b1bee7ce77d794b57689e1a4b2fc4d
mikebucher <mike.bucher@digitecgalaxus.ch>
2022-12-01 09:21:14 +0100

    Day1 done

---
+++ Day1/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] lines = File.ReadAllLines(@"../../../input.txt");
+List<int> totalCalories = new List<int>();
+var current = 0;
+
+foreach (var line in lines)
+{
+    if(line != String.Empty) current += Int32.Parse(line);
+    else
+    {
+        totalCalories.Add(current);
+        current = 0;
+    }
+}
+
+var sorted = totalCalories.OrderByDescending(x => x);
+
+var result = sorted.Take(3).Sum();
+
+Console.WriteLine($"Total Calories of Top 3: {result}");
