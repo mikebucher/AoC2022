a090d22335239e3da8e52e9d90ce43e61c446dcd
mikebucher <mike.bucher@digitecgalaxus.ch>
2022-12-02 06:59:43 +0100

    day2 done, mega not nice

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,1 +1,54 @@
 ﻿string[] lines = File.ReadAllLines(@"../../../input.txt");
+int result = 0;
+
+foreach (var line in lines)
+{
+    var split = line.Split(" ");
+    switch (split[0])
+    {
+        case "A":
+            switch (split[1])
+            {
+                case "X":
+                    result += 3;
+                    break;
+                case "Y":
+                    result += 4;
+                    break;
+                case "Z":
+                    result += 8;
+                    break;
+            }
+            break;
+        case "B":
+            switch (split[1])
+            {
+                case "X":
+                    result += 1;
+                    break;
+                case "Y":
+                    result += 5;
+                    break;
+                case "Z":
+                    result += 9;
+                    break;
+            }
+            break;
+        case "C":
+            switch (split[1])
+            {
+                case "X":
+                    result += 2;
+                    break;
+                case "Y":
+                    result += 6;
+                    break;
+                case "Z":
+                    result += 7;
+                    break;
+            }
+            break;
+    }
+}
+
+Console.WriteLine($"Result: {result}");
