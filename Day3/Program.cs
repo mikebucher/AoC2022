34510cc0801d437aa62c5f4f97e868f100171f25
mikebucher <mike.bucher@digitecgalaxus.ch>
2022-12-03 10:22:30 +0100

    day3 done

---
+++ Day3/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] lines = File.ReadAllLines(@"../../../input.txt");
+int result = 0;
+
+for (int i = 0; i < lines.Length; i+=3)
+{
+    List<char> temp = new List<char>();
+    foreach (char c in lines[i])
+    {
+        if (lines[i+1].Contains(c) && lines[i+2].Contains(c) && !temp.Contains(c))
+        {
+            result += c % 32;
+            if (Char.IsUpper(c)) result += 26;
+            temp.Add(c);
+        }
+    }
+}
+
+Console.WriteLine($"Result Part2: {result}");
