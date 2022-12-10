960a5284a2e51471e41a2a953bcef260c8e7a580
mikebucher <mike.bucher@digitecgalaxus.ch>
2022-12-10 06:47:07 +0100

    day10 done

---
+++ Day10/Program.cs
@@ -0,0 +1,37 @@
+﻿var lines = File.ReadAllLines(@"../../../input.txt");
+var cycle = 0;
+var x = 1;
+var crt = new char[240];
+
+foreach (var t in lines)
+{
+    var split = t.Split(' ');
+    switch (split[0])
+    {
+        case "noop":
+            cycle++;
+            CheckForRelevantCycle();
+            break;
+        case "addx":
+            cycle++;
+            CheckForRelevantCycle();
+            cycle++;
+            CheckForRelevantCycle();
+            x += Convert.ToInt32(split[1]);
+            break;
+    }
+}
+
+Console.WriteLine($"{new string(crt)}");
+
+void CheckForRelevantCycle()
+{
+    if (cycle % 40 -1 >= x - 1 && cycle % 40 -1 <= x + 1)
+    {
+        crt[cycle-1] = '#';
+    }
+    else
+    {
+        crt[cycle-1] = '.';
+    }
+}
